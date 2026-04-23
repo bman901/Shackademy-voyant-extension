5881dd4b9a48d1153dbed837db9bda99ebc608ef
bman901 <b_man901@hotmail.com>
2026-04-23 15:04:17 +1200

    Adding additional tooltips to fields & updating lessons with correct links (still need to review which lessons show where)

--- lessons.js
+++ lessons.js
@@ -13,7 +13,7 @@ window.SHACKADEMY_LESSONS = {
 
   "intro-to-voyant": {
     title: "Getting started with Voyant",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=intro",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=67734dec1c6b5ca11b094f0cUnit",
   },
 
   "people-basics": {
@@ -23,67 +23,72 @@ window.SHACKADEMY_LESSONS = {
 
   "income-basics": {
     title: "Adding employment income",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=income-basics",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773147b8f48f0512d0bc578Unit",
   },
 
   "other-income": {
     title: "Adding other income",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=other-income",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773147b8f48f0512d0bc578Unit",
+  },
+  
+  "expenses": {
+    title: "Adding your expenditure/goals",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773147b8f48f0512d0bc578Unit",
   },
 
   "pension-basics": {
     title: "Adding a defined contribution pension",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773147b8f48f0512d0bc578Unit",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773214bcbc96cfe9f04a018Unit",
   },
 
   "db-pension-basics": {
     title: "Adding a defined benefit (final salary) pension",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=db-pension",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773214bcbc96cfe9f04a018Unit",
   },
 
   "property-basics": {
     title: "Adding a property to your plan",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=property-basics",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773214dd5c00b37cd01cf96Unit",
   },
 
   "savings-basics": {
-    title: "Adding savings accounts and cash ISAs",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=savings-basics",
+    title: "Adding savings accounts",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773215dff7e551718002c16Unit",
   },
 
   "investments-basics": {
     title: "Adding investments and stocks & shares ISAs",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=investments-basics",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773215dff7e551718002c16Unit",
   },
 
   "what-if-scenarios": {
     title: "Using What If scenarios",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=what-if",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=67562126bb11d632650d471aUnit",
   },
 
   "understanding-the-chart": {
-    title: "Reading the Voyant chart",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=chart",
+    title: "Reading the dashboard",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=678ed75a2b68d60a6c0a38caUnit",
   },
 
   "liquidation-order": {
-    title: "Understanding the liquidation order",
+    title: "Understanding the liquidation order [remove]",
     url: "https://shackademy.com/path-player?courseid=voyant&unit=liquidation-order",
   },
 
   "contributions-guide": {
     title: "Setting up contributions",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=contributions",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=690bc5f12557485ba40f9aa7Unit",
   },
 
   "planned-withdrawals": {
     title: "Planned withdrawals and withdrawal limits",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=withdrawals",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=67c8e58056c0924b390c507eUnit",
   },
 
   "timeline-events": {
     title: "Working with timeline events",
-    url: "https://shackademy.com/path-player?courseid=voyant&unit=timeline",
+    url: "https://shackademy.com/path-player?courseid=voyant&unit=6773147e337de51fe604f62aUnit",
   },
 
 };
